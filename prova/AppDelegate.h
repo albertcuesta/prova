722c4b99f595b113739d476084deabf787150567
Albert <albert@MacBook-Pro-de-Albert.local>
2015-01-26 00:37:45 +0100

    Initial Commit

---
+++ prova/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prova
+//
+//  Created by Albert on 26/1/15.
+//  Copyright (c) 2015 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ prova/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  prova
+//
+//  Created by Albert on 26/1/15.
+//  Copyright (c) 2015 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
